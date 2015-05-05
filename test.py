a7891bbe1b401b513d7e3ed12656488066dd4f7e
Zephyros <dieinthewind@gmail.com>
2015-05-05 01:39:56 -0400

    score

--- test.py
+++ test.py
@@ -1,15 +1,5 @@
 __author__ = 'zephyros'
 import os
 import nltk.tokenize
-def Test2(rootDir):
-    for lists in os.listdir(rootDir):
-        path = os.path.join(rootDir, lists)
-        print(path)
-        # if os.path.isdir(path):
-        #     Test2(path)
+import DIR
 
-Test2('./d04aNER/')
-
-sen = 'hello this is jack'
-words = nltk.tokenize.word_tokenize(sen)
-print(words)
--- useOpenIE.py
+++ useOpenIE.py
@@ -5,6 +5,7 @@ from Article import Article
 from NameEntity import NE
 import nltk.tokenize
 import math
+import DIR
 
 def loopDir(rootDir):
     d = []
@@ -20,46 +21,45 @@ def setScore(words, scores):
             score += scores[word]
     return score
 
-sentenceScores = {}
+DIR.mkdir('openieScoreSummary')
+paths = DIR.loopShallowDir('openieOutput')
+for p in paths:
+    name = p.split('/')[1]
+    print(name)
+    sentenceScores = {}
 
-fileNames = ['d04aOpenieResult/' + str(i+1) + '.txt' for i in range(7)]
-articles = [Article(f) for f in fileNames]
-articleWords = [article.wordFrequency for article in articles]
+    fileNames = DIR.loopShallowDir(p)
+        #['d04aOpenieResult/' + str(i+1) + '.txt' for i in range(7)]
+    articles = [Article(f) for f in fileNames]
+    articleWords = [article.wordFrequency for article in articles]
 
-ne = NE(loopDir('./d04aNER'))
-t = ScoreWords(articleWords, ne.dict)
-scores = t.getScore()
+    ne = NE(DIR.loopShallowDir('ner/' + name))
+    t = ScoreWords(articleWords, ne.dict)
+    scores = t.getScore()
 
-tmp = sorted(scores.items(), key=lambda d :d[1], reverse=True)
-# for s in tmp:
-#     print(s)
+    tmp = sorted(scores.items(), key=lambda d :d[1], reverse=True)
 
+    for article in articles:
+        for i in range(len(article.content)):
+            line = article.content[i]
+            words = article.extractionWords[i]
+            score = setScore(words, scores)
+            sentenceScores[line] = score
 
+    print_entity = sorted(sentenceScores.items(), key=lambda d :d[1]/math.log(len(nltk.tokenize.word_tokenize(d[0])),2), reverse=True)
+    #print_entity = sorted(sentenceScores.items(), key=lambda d :d[1], reverse=True)
 
-for article in articles:
-    for i in range(len(article.content)):
-        line = article.content[i]
-        words = article.extractionWords[i]
-        score = setScore(words, scores)
-        sentenceScores[line] = score
-
-
-print_entity = sorted(sentenceScores.items(), key=lambda d :d[1]/math.log(len(nltk.tokenize.word_tokenize(d[0])),2), reverse=True)
-#print_entity = sorted(sentenceScores.items(), key=lambda d :d[1], reverse=True)
-
-for p in print_entity:
-    print(p)
-summary = ''
-cnt = 0
-lineCnt = 0
-while cnt < 200:
-    line = print_entity[lineCnt][0]
-    summary += line + ' '
-    lineCnt += 1
-    cnt += len(nltk.tokenize.word_tokenize(line))
-file = open('summary.txt','w')
-file.write(summary)
-file.close()
+    summary = ''
+    cnt = 0
+    lineCnt = 0
+    while cnt < 200:
+        line = print_entity[lineCnt][0]
+        summary += line + ' '
+        lineCnt += 1
+        cnt += len(nltk.tokenize.word_tokenize(line))
+    file = open('openieScoreSummary/' + name + '.txt','w')
+    file.write(summary)
+    file.close()
 
 
 
